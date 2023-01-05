68072b4fb068600c915214e825182d3c04bc909d
vicky9168 <dholevivek1111@gmail.com>
2023-01-05 19:58:09 +0530

    create .env file

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import cors from 'cors'
-// import dotenv from 'dotenv'
-import path, { dirname } from 'path'
+import dotenv from 'dotenv'
+import path from 'path'
 import { fileURLToPath } from 'url';
 
 import userRoutes from './routes/users.js'
@@ -10,11 +10,12 @@ import questionRoutes from './routes/Questions.js'
 import answerRoutes from './routes/Answers.js'
 
 const app=express()
-// dotenv.config();
+dotenv.config();
 
 const __filename = fileURLToPath(import.meta.url);
 
 const __dirname = path.dirname(__filename);
+
 app.use(express.json({limit:'30mb',extended:true}))
 app.use(express.urlencoded({limit:'30mb',extended:true}))
 app.use(cors());
@@ -38,7 +39,7 @@ app.use('/answer',answerRoutes)
 
 const PORT=process.env.PORT || 5000;
 
-const DATABASE_URL = "mongodb+srv://admin-V:admin%4012@stack-overflow-clone.1diigfn.mongodb.net/?retryWrites=true&w=majority";
+const DATABASE_URL = process.env.CONNECTION_URL ;
 mongoose.set('strictQuery',true);
 
 mongoose.connect(DATABASE_URL,{ useNewUrlParser:true})
